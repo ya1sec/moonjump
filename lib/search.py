4746eaa90e686809e3a448e97b7d45b391ca28ab
Ankit Bagchi <y1s@Ankits-MBP.fios-router.home>
2022-06-17 17:30:22 -0400

    add marginalia search and homepage

--- lib/search.py
+++ lib/search.py
@@ -1,26 +1,6 @@
 from lib.helpers import *
 import requests
 
-
-# # Search marginalia api with query
-# def search(query):
-# 	url = "https://api.marginalia.nu/{key}/search/{query}"
-# 	response = requests.get(url.format(key='public', query=query))
-
-# 	if response.ok:
-# 		return response.json()
-# 	else:
-# 		return None
-
-# def process_search(query):
-# 	data = search(query)
-# 	if data:
-# 		results = data['results']
-# 		random = random.choice(results)
-# 		return random['url']
-# 	else:
-# 		return None
-
 class Search:
 	def __init__(self):
 		self.base_url = 'https://api.marginalia.nu/public/search'
